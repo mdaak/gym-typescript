3dbfb25523701a451c5e1ac709df910408686f7d
mdaak <md.aak3@gamil.com>
2023-02-12 01:38:35 +0600

    1.22 time span

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
-import Navbar from './scenes/navbar';
-
+import Navbar from '@/scenes/navbar';
+import { SelectedPage } from '@/shared/types';
 
 
 function App() {
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
 
   return (
     <div className="app bg-gray-20">
-    <Navbar/>
+    <Navbar 
+     selectedPage={selectedPage}
+     setSelectedPage={setSelectedPage}
+    />
     </div>
   )
 }
